caaac1322a0b691902aec0247d6459326b810963
Justin Chan <justinjc@outlook.com>
2014-04-18 16:20:07 -0400

    Before dropping

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(function() {
+
 var BOARD_HEIGHT = 21;
 var BOARD_WIDTH = 13;
 
@@ -5,25 +7,51 @@ var ACTION = {
     DOWN: 0,
     LEFT: 1,
     RIGHT: 2,
-    ROTATE: 3
+    ROTATE: 3,
+    DROP: 4
 };
 
 var tickSpeed = 1000;
 var gameTicker = false;
 
 var activePieces = [];
-var nextPieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+var nextPieces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+var cellTaken = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
 var currentPiece;
 
 /*
- * Piece
- */
+** BEGIN Piece
+**/
 // Piece constructor.
 // args is either {'type' : t, 'structure' : [...]} or {'type' : t, 'orientation' : o}
 function Piece(x, y, args) {
     this.x = x;
     this.y = y;
+    this.placed = 0;
     
     if (args.structure) {
         this.type = args.type;
@@ -64,12 +92,14 @@ Piece.prototype.store = function() {
     activePieces.push(this);
 };
 
+// Performs an action. Returns 1 if something significant happens
 Piece.prototype.move = function(action) {
     if (!this.collides(action)) {
         this.draw(false);
         
         switch (action) {
             case ACTION.DOWN:
+            case ACTION.DROP:
                 this.y++;
                 break;
             case ACTION.LEFT:
@@ -85,49 +115,50 @@ Piece.prototype.move = function(action) {
         }
         
         this.draw(true);
-    } else if (action == ACTION.DOWN) { // Hit something going down
-        this.mark(true);
-        this.store();
         
-        var fullLines = getFullLines();
-        if (fullLines.length > 0) {
-            for (var i = 0; i < activePieces.length; i++) {
-                activePieces[i].draw(false);
-                activePieces[i].mark(false);
-                activePieces[i].clearLines(fullLines);
-            }
+        return 1;
+    } else { // Collides
+        if (action == ACTION.DOWN) { // Hit something moving down
+            this.mark(true);
+            this.store();
             
-            for (var i = 0; i < activePieces.length; i++) {
-                activePieces[i].draw(true);
-                activePieces[i].mark(true);
-            }
+            checkAndClear();
+            
+            issueNewPiece();
+            writeDebug();
+            
+            return 1;
+        } else if (action == ACTION.DROP) { // Hit something going down as a result of a cleared line
+            return checkAndClear();
         }
-        
-        issueNewPiece();
-        writeDebug();
     }
 };
 
+Piece.prototype.down = function() {
+    return this.move(ACTION.DOWN);
+};
 Piece.prototype.left = function() {
-    this.move(ACTION.LEFT);
+    return this.move(ACTION.LEFT);
 };
 Piece.prototype.right = function() {
-    this.move(ACTION.RIGHT);
-};
-Piece.prototype.down = function() {
-    this.move(ACTION.DOWN);
+    return this.move(ACTION.RIGHT);
 };
 Piece.prototype.rotate = function() {
-    this.move(ACTION.ROTATE);
+    return this.move(ACTION.ROTATE);
+};
+Piece.prototype.drop = function() {
+    return this.move(ACTION.DROP);
 };
 
+// Returns true if performing an action would result in a collision
 Piece.prototype.collides = function(action) {
-    var testStructure = this.structure;
+    var testStructure = clone(this.structure);
     var testX = this.x;
     var testY = this.y;
 
     switch (action) {
         case ACTION.DOWN:
+        case ACTION.DROP:
             testY++;
             break;
         case ACTION.LEFT:
@@ -156,8 +187,10 @@ Piece.prototype.collides = function(action) {
     return false;
 };
 
+// Clear lines. Argument is in terms of the game grid. Returns true if cleared lines
 Piece.prototype.clearLines = function(linesToClear) {
     var lines = clone(linesToClear);
+    var clearedLines = 0;
 
     // Get lines relative to this.structure
     for (var l = 0; l < lines.length; l++) {
@@ -166,6 +199,7 @@ Piece.prototype.clearLines = function(linesToClear) {
     
     for (var j = this.structure.length - 1; j >= 0; j--) {
         if ($.inArray(j, lines) > -1) {
+            clearedLines = 1;
             var newPieceStructure = this.structure.splice(j);
             
             if (newPieceStructure.length > 1) {
@@ -176,12 +210,66 @@ Piece.prototype.clearLines = function(linesToClear) {
             }
         }
     }
+    
+    return clearedLines;
+};
+
+Piece.prototype.isEmpty = function() {
+    return !this.structure.length;
 };
 /*
- * END Piece
- */
- 
-// Clones a 1/2D array
+** END Piece
+**/
+
+// Check the game grid for full lines and clear them
+function checkAndClear() {
+    var fullLines = getFullLines();
+    
+    if (fullLines.length > 0) {
+        for (var i = 0; i < activePieces.length; i++) {
+            activePieces[i].draw(false);
+            activePieces[i].mark(false);
+            activePieces[i].clearLines(fullLines);
+        }
+        
+        // There may be new active pieces, do this in a separate loop
+        for (i = 0; i < activePieces.length; i++) {
+            activePieces[i].draw(true);
+            activePieces[i].mark(true);
+        }
+        
+        removeEmptyPieces();
+        
+        return 1;
+    }
+    
+    return 0;
+}
+
+// Drop all pieces until they all hit the bottom
+function dropPieces() {
+    var somethingHappened = 1;
+    
+    while (somethingHappened) {
+        somethingHappened = 0;
+        for (var p = 0; p < activePieces.length; p++) {
+            if (activePieces[p].drop()) {
+                somethingHappened = 1;
+            }
+        }
+    }
+}
+
+// Removes the pieces from activePieces that have an empty structure
+function removeEmptyPieces() {
+    for (var i = 0; i < activePieces.length; i++) {
+        if (activePieces[i].isEmpty()) {
+            activePieces.splice(i, 1);
+        }
+    }
+}
+
+// Clones an array
 function clone(arr) {
     var cloneArr = arr.slice(0);
     
@@ -205,30 +293,6 @@ function markCell(x, y, reallyMark) {
 function colorCell(x, y, color) {
     $($('#gameTable>tbody').children()[y].children[x]).css('background', color);
 }
-
-var cellTaken = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0]
-];
 
 function issueNewPiece() {
     var newPieceType = Math.floor(Math.random() * 18) + 1;
@@ -267,14 +331,6 @@ function startNewGame() {
     }
 }
 
-function paintBoard() {
-    for (var j = 0; j < BOARD_HEIGHT; j++) {
-        for (var i = 0; i < BOARD_WIDTH; i++) {
-            colorCell(i, j, PIECE_COLOR[gameBoard[j][i][1]]);
-        }
-    }
-}
-
 function clearBoard() {
     for (var j = 0; j < BOARD_HEIGHT; j++) {
         for (var i = 0; i < BOARD_WIDTH; i++) {
@@ -282,6 +338,8 @@ function clearBoard() {
             colorCell(i, j, PIECE_COLOR[0]);
         }
     }
+    
+    activePieces = [];
 }
 
 function permutePieces(pieces) {
@@ -319,34 +377,34 @@ function writeDebug() {
     }
 }
 
-$(function() {
-    $('#startBtn').click(function() {
-        startNewGame();
-    });
+$('#startBtn').click(function() {
+    startNewGame();
+});
 
-    $('#endBtn').click(function() {
-        endGame();
-    });
+$('#endBtn').click(function() {
+    endGame();
+});
 
-    $('html').keydown(function(event) {
-        switch (event.keyCode) {
-            case 38: // UP
-                currentPiece.rotate();
-                break;
-            case 37: // LEFT
-                currentPiece.left();
-                break;
-            case 40: // DOWN
-                currentPiece.down();
-                break;
-            case 39: // RIGHT
-                currentPiece.right();
-                break;
-            case 13: // ENTER
-                startNewGame();
-                break;
-        }
-        //$('#debug').html($('#debug').html() + ", " + event.keyCode);
-        
-    });
+$('html').keydown(function(event) {
+    switch (event.keyCode) {
+        case 38: // UP
+            currentPiece.rotate();
+            break;
+        case 37: // LEFT
+            currentPiece.left();
+            break;
+        case 40: // DOWN
+            currentPiece.down();
+            break;
+        case 39: // RIGHT
+            currentPiece.right();
+            break;
+        case 13: // ENTER
+            startNewGame();
+            break;
+    }
+    //$('#debug').html($('#debug').html() + ", " + event.keyCode);
+    
+});
+
 });
