f2441628e8f82c7c1f2cec6308fc89f2f26fb5dd
30yearsofapples <cyrichard418@gmail.com>
2024-06-04 10:28:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1196,7 +1196,7 @@ function getNextPiece() {
 
     if (nextPieces.length === 0) {
         nextPieces = nextNextPieces;
-        nextNextPieces = permutePieces([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
+        nextNextPieces = permutePieces([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
     }
 
     return nextPiece;
